0cf2e97720a6543d4c49f3278dc69d38cf06eafa
mogmet <mogmet@gmail.com>
2020-04-10 23:43:04 +0900

    [add] initial commit

---
+++ danger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  danger
+//
+//  Created by mogmet on 2020/04/10.
+//  Copyright © 2020 mogmet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
